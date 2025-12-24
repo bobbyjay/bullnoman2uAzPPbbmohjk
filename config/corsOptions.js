2959856320886627cb32b9a60435d2270334c7b3
bobbyjay <jaymuhammed401@gmail.com>
2025-12-24 06:47:14 +0100

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "https://clutchden.com",
   "https://www.clutchden.com",
   "https://clutchden.onrender.com",
+  "https://a-q7-m9x-l2-k8-zp-4-hn-e5-r6d-yb-3w-pi.vercel.app",
   "http://localhost:3000",
   "http://localhost:5173"
 ];
